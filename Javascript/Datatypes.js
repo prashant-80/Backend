989f40ca431a88b95a0fa68f8501e11f6c62cdba
Prashant singh <121568016+prashant-80@users.noreply.github.com>
2023-12-23 15:23:32 +0000

    3rd commit

---
+++ Javascript/Datatypes.js
@@ -0,0 +1,13 @@
+var name=undefined;  //something not defined yes but maybe defined
+var age=null;
+var isMale=false;
+var profile="student";
+var num=20;
+var data={
+    name:"varun",
+    age:25,
+    proffesion:"student"
+}
+
+console.log(data);
+
---
+++ Javascript/Operators.js
@@ -0,0 +1,10 @@
+//arithmatic operator
+var x=20;
+var y=20;
+console.log(x+y);
+console.log(x-y);
+console.log(x/y);
+console.log(x%y);
+//assignment operator
+//relational operator 
+//logical operator
