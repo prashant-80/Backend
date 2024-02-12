b6d7c02b7ed741b17efb5892bba65848b39c41e9
Prashant singh <121568016+prashant-80@users.noreply.github.com>
2024-02-12 16:00:54 +0000

    scopes

---
+++ Javascript/Scopes1.js
@@ -0,0 +1,9 @@
+if(true){
+    var x =1;  //var only supports global scope and function scope
+}
+{
+    let a =10;  //supports block scopes //it is only be accesible below it declaration
+}
+
+console.log(x);
+console.log(a);  //error
